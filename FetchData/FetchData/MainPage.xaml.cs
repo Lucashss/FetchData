5e7741179fbbd1b01874b7fddf1dd7a0753dddfb
Lucas <lucas.hsouza@hotmail.com>
2022-10-24 00:05:54 -0300

    Adicionar arquivos de projeto.

---
+++ FetchData/FetchData/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace FetchData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Login();
+        }
+
+        public async void Login()
+        {
+
+            var httpClient = new HttpClient();
+            var reponse = await httpClient.GetStringAsync("https://localhost:44397/api/Logins1");
+            var login = JsonConvert.DeserializeObject<List<Login>>(reponse);
+            ListLogin.ItemsSource = login;
+
+        }
+    }
+}
